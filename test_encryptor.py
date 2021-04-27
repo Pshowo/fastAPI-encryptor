096110abc3f9fbf45bebf6de5aca8dfc9aa02148
Pawel <pjuszcze@gmail.com>
2021-04-27 22:49:25 +0200

    Changed: test to test the class

--- test_encryptor.py
+++ test_encryptor.py
@@ -1,6 +1,24 @@
-from vigenere.encryptor import encrypt
+from vigenere.encryptor import Vigenere
+import unittest
+# def test_encrypt():
+#     # Encrypting word
+#     assert encrypt("BEDE", "DAFE") == "EECC"
+#     assert encrypt("BACA", "DAFE") == "EECC"
+
+def test_vigenere_init():
+    v = Vigenere()
+    assert v
+
+def test_table():
+    v = Vigenere()
+    assert """[['A' 'B' 'C' 'D' 'E' 'F']
+ ['B' 'C' 'D' 'E' 'F' 'A']
+ ['C' 'D' 'E' 'F' 'A' 'B']
+ ['D' 'E' 'F' 'A' 'B' 'C']
+ ['E' 'F' 'A' 'B' 'C' 'D']
+ ['F' 'A' 'B' 'C' 'D' 'E']]"""
 
 def test_encrypt():
-    # Encrypting word
-    assert encrypt("BEDE", "DAFE") == "EECC"
-    assert encrypt("BACA", "DAFE") == "EECC"
+    v = Vigenere()
+    assert v.encrypt("BEDE", "DAFE") == "EECC"
+    assert v.encrypt("BACA", "DAFE") == "EABE"
