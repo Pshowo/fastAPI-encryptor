4715b4d01191a3d5090b6d69bdefbfc726adaa39
Pawel <pjuszcze@gmail.com>
2021-04-27 23:50:31 +0200

    Added: decrypt

--- vigenere/encryptor.py
+++ vigenere/encryptor.py
@@ -6,6 +6,7 @@ class Vigenere:
     def __init__(self):
         self.alphabet = np.array(['A', 'B', 'C', 'D', 'E', 'F'])
         self.table = self.table()
+        self.key = ""
 
     def __str__(self):
         return str(self.table)
@@ -23,7 +24,7 @@ class Vigenere:
             table = np.append(table, [np.roll(self.alphabet, -i)], axis=0)
         return table
 
-    def encrypt(self, msg):
+    def encrypt(self, msg, **kwargs):
         """Encrypts message using by Vigenere method. 
 
         Parameters
@@ -37,11 +38,26 @@ class Vigenere:
             Touple with encrypted message and the key which a message will be encrypted.
         """
         closed_msg = ""
-        key = "".join(random.choice(self.alphabet) for _ in range(len(msg)))
-        assert len(msg) == len(key), "Wrong length message or key."
+        if 'key' in kwargs:
+            self.key = kwargs['key']
+        else:
+            self.key = "".join(random.choice(self.alphabet) for _ in range(len(msg)))
+        assert len(msg) == len(self.key), "Wrong length message or key."
         
         for char in range(len(msg)):
             x0 = np.argwhere(self.table[0] == msg[char])
-            x1 = np.argwhere(self.table[:, [0]] == key[char])
+            x1 = np.argwhere(self.table[:, [0]] == self.key[char])
             closed_msg += self.table[x0[0][0]][x1[0][0]]
-        return closed_msg, key
+        return closed_msg, self.key
+
+    def decrypt(self, c_msg, key):
+        open_msg = ""
+        for char in range(len(c_msg)):
+            x1 = np.argwhere(self.table[:, [0]] == key[char])
+            x0 = np.argwhere(self.table[x1[0][0]] == c_msg[char])
+            open_msg += self.table[x0[0][0]][0]
+        return open_msg
+
+v = Vigenere()
+var = v.encrypt("BACA", key="DAFE")
+print(v.decrypt(var[0], var[1]))
