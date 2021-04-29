2a5bd73494a4b482f7a267bf7ebf34529ae48ddc
Pawel <pjuszcze@gmail.com>
2021-04-30 00:09:50 +0200

    Changed: reformat

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ from vigenere.encryptor import Vigenere
 from pydantic import BaseModel
 from fastapi import FastAPI
 from typing import Optional
+import string
 from vigenere import *
 import vigenere
 
-v = Vigenere()
+v = Vigenere(string.printable)
 
 tags_metadata = [
     {
@@ -22,8 +23,9 @@ tags_metadata = [
     },
 ]
 
+
 class Message(BaseModel):
-    msg:str
+    msg: str
     key: Optional[str]
 
 app = FastAPI(
@@ -37,6 +39,7 @@ VER = '/v1'
 fake_msg = {
     0: "fake msg"
 }
+
 
 def append_message(open_msg, close_msg, key):
     global fake_msg
@@ -55,12 +58,14 @@ async def root():
         "cipher_vigenere": "/v1/vigenere"
         }
 
+
 @app.get(VER + "/vigenere", tags=['cipher'])
 async def vigenere():
     return {
         "encryptor": "/encryptor",
         "decryptor": "/decryptor"
     }
+
 
 @app.post(VER + "/vigenere/encryptor", tags=['encryptor'])
 async def vigenere_encryptor(open_msg: Message):
@@ -78,6 +83,7 @@ async def vigenere_encryptor(open_msg: Message):
     close_msg, key = v.encrypt(open_msg.msg)
     append_message(open_msg.msg, close_msg, key)
     return fake_msg
+
 
 @app.post(VER + "/vigenere/decryptor", tags=['decryptor'])
 async def vigenere_decryptor(closed_msg: Message):
