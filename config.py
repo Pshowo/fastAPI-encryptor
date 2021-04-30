8c2a23b00097f9603271a2fe2b3c29897bd44bc8
Pawel <pjuszcze@gmail.com>
2021-04-30 20:22:23 +0200

    Added: Basic authentication and some docs

---
+++ config.py
@@ -0,0 +1,19 @@
+PL_ext = "ąćęłńóśźżĄĘĆĘŁŃÓŚŹŻ"
+latn_ext_A = "ĀāĂăĄąąĆćĈĉĊċČčĎďĐđĒēĔĕĖėĘęĚěĜĝĞğĠġĢģĤĥĦħĨĩĪīĬĭĮįİıĲĳĴĵĶķĸĹĺĻļĽľĿŀŁłŃńŅņŇňŉŊŋŌōŎŏŐőŒœŔŕŖŗŘřŚśŜŝŞşŠšŢţŤťŦŧŨũŪūŬŭŮůŰűŲųŴŵŶŷŸŹźŻżŽžſ"
+USER = "admin"
+PASS = "qwerty"
+
+tags_metadata = [
+    {
+        "name": "cipher",
+        "description": "Inculde encrypt and decrypt methods.",
+    },
+    {
+        "name": "encryptor",
+        "description": "POST message which you want to encode.",
+    },
+    {
+        "name": "decryptor",
+        "description": "POST close message and key to decode it.",
+    },
+]
--- main.py
+++ main.py
@@ -1,33 +1,21 @@
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+from fastapi import FastAPI, Depends, HTTPException, status
 from vigenere.encryptor import Vigenere
 from pydantic import BaseModel
-from fastapi import FastAPI
 from typing import Optional
+from config import *
+import secrets
 import string
-from vigenere import *
-import vigenere
 
-v = Vigenere(string.printable)
-
-tags_metadata = [
-    {
-        "name": "cipher",
-        "description": "Inculde encrypt and decrypt methods.",
-    },
-    {
-        "name": "encryptor",
-        "description": "POST message which you want to encode.",
-    },
-    {
-        "name": "decryptor",
-        "description": "POST close message and key to decode it.",
-    },
-]
+v = Vigenere(string.printable+PL_ext)
 
 
 class Message(BaseModel):
     msg: str
     key: Optional[str]
 
+
+security = HTTPBasic()
 app = FastAPI(
     title="fastAPI enctryptor",
     description="API with cipher algorithm to encrypt and decrypt a message using by Vigenere method.",
@@ -40,13 +28,24 @@ fake_msg = {
     0: "fake msg"
 }
 
+def get_current_username(credentials: HTTPBasicCredentials = Depends(security)):
+    correct_username = secrets.compare_digest(credentials.username, USER)
+    correct_password = secrets.compare_digest(credentials.password, PASS)
+    if not (correct_username and correct_password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect user or password",
+            headers={"WWW-Authenticate": "Basic"},
+        )
+    return credentials.username
+
 
 def append_message(open_msg, close_msg, key):
     global fake_msg
     fake_msg[max(fake_msg,key=int)+1] = {
         "open_msg": open_msg,
         "key": key,
-        "close_msg": close_msg }
+        "close_msg": close_msg}
 
 
 @app.get("/",  tags=['cipher'])
@@ -55,20 +54,29 @@ async def root():
         "version": "1.0",
         "name": "fastAPI-encryptor",
         "docs": "/docs",
-        "cipher_vigenere": "/v1/vigenere"
+        "cipher_vigenere": "/v1/vigenere",
+        "avaiable_chars": v.raw_alphabet
         }
 
 
 @app.get(VER + "/vigenere", tags=['cipher'])
 async def vigenere():
+    """
+    Shows available methods.
+    """
     return {
-        "encryptor": "/encryptor",
-        "decryptor": "/decryptor"
+        "encryptor": "/v1/vigenere/encryptor",
+        "decryptor": "/v1/vigenere/decryptor"
     }
 
+# class Item(BaseModel):
+#     name: str
+#     description: Optional[str] = None
+#     price: float
+#     # tax: Optional[float] = None
 
 @app.post(VER + "/vigenere/encryptor", tags=['encryptor'])
-async def vigenere_encryptor(open_msg: Message):
+async def vigenere_encryptor(open_msg: Message, q: Optional[str] = None, username: str = Depends(get_current_username)):
     """Encoding messege.
 
     **open_msg** - contain only message.
@@ -80,13 +88,18 @@ async def vigenere_encryptor(open_msg: Message):
     - **key** - key to decode encrypted message
     - **close_msg** - encrypted message
     """
-    close_msg, key = v.encrypt(open_msg.msg)
-    append_message(open_msg.msg, close_msg, key)
-    return fake_msg
+    if q:
+        close_msg, key = v.encrypt(q)
+        append_message(q, close_msg, key)
+        return fake_msg
+    else:
+        close_msg, key = v.encrypt(open_msg.msg)
+        append_message(open_msg.msg, close_msg, key)
+        return fake_msg
 
 
 @app.post(VER + "/vigenere/decryptor", tags=['decryptor'])
-async def vigenere_decryptor(closed_msg: Message):
+async def vigenere_decryptor(closed_msg: Message, username: str = Depends(get_current_username)):
     """Decoding message:
     
     **closed_msg** - must contain closed message and key
